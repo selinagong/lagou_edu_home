a918d137045d202a8d02352502954e1be163aa98
gongsainan <18510273307@163.com>
2023-04-26 21:33:55 +0800

    commit

---
+++ lagou_edu_home_parent/ssm_dao/src/main/java/com/lagou/dao/CourseMapper.java
@@ -0,0 +1,13 @@
+package com.lagou.dao;
+
+import com.lagou.domain.Course;
+import com.lagou.domain.CourseVO;
+
+import java.util.List;
+
+public interface CourseMapper {
+
+    //根据条件查询课程信息
+    //public List<Course> findAllCourse(CourseVO courseVO);
+    public List<Course> findAllCourse();
+}
--- lagou_edu_home_parent/ssm_service/src/main/java/com/lagou/Main.java
+++
@@ -1,7 +0,0 @@
-package com.lagou;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
