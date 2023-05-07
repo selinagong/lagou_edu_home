4054600c32e03d1802dac67be76aba54b71e2f30
gongsainan <18510273307@163.com>
2023-05-07 15:50:16 +0800

    commit

--- lagou_edu_home_parent/ssm_dao/src/main/java/com/lagou/dao/ResourceMapper.java
+++ lagou_edu_home_parent/ssm_dao/src/main/java/com/lagou/dao/ResourceMapper.java
@@ -1,4 +1,12 @@
 package com.lagou.dao;
 
 public interface ResourceMapper {
+
+    // 资源信息分页&条件查询 findAllResource
+
+    // 添加资源信息  saveResource
+
+    // 更新资源信息  updateResource
+
+    // 删除资源信息 deleteResource
 }
